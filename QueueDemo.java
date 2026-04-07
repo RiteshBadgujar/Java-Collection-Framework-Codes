e7f92b10c6e8645be08fdd7a4084866b69822efb
RiteshBadgujar <riteshbadgujar0@gmail.com>
2026-04-07 22:48:38 +0530

    [ADDED]:Ritesh -:The add element in queue and after remove some element then print all element program.

---
+++ QueueDemo.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class QueueDemo {
+    public static void main(String[] args) {
+
+        Queue<Integer> q = new LinkedList<>();
+
+        q.add(10);
+        q.add(20);
+        q.add(30);
+
+        System.out.println("Queue: " + q);
+
+        System.out.println("Removed: " + q.poll()); // removes first
+        System.out.println("After removal: " + q);
+    }
+}
